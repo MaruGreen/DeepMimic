5aedd9ad3d510e919d131c98e88bf9d4d1cf08a7
Maru Green <37184942+MaruGreen@users.noreply.github.com>
2023-05-29 20:19:03 +0800

    Reformat

--- util/mpi_util.py
+++ util/mpi_util.py
@@ -4,36 +4,46 @@ from mpi4py import MPI
 
 ROOT_PROC_RANK = 0
 
+
 def get_num_procs():
     return MPI.COMM_WORLD.Get_size()
 
+
 def get_proc_rank():
     return MPI.COMM_WORLD.Get_rank()
+
 
 def is_root_proc():
     rank = get_proc_rank()
     return rank == ROOT_PROC_RANK
 
+
 def bcast(x):
     MPI.COMM_WORLD.Bcast(x, root=ROOT_PROC_RANK)
     return
 
+
 def reduce_sum(x):
     return reduce_all(x, MPI.SUM)
 
+
 def reduce_prod(x):
     return reduce_all(x, MPI.PROD)
+
 
 def reduce_avg(x):
     buffer = reduce_sum(x)
     buffer /= get_num_procs()
     return buffer
 
+
 def reduce_min(x):
     return reduce_all(x, MPI.MIN)
 
+
 def reduce_max(x):
     return reduce_all(x, MPI.MAX)
+
 
 def reduce_all(x, op):
     is_array = isinstance(x, np.ndarray)
@@ -43,14 +53,15 @@ def reduce_all(x, op):
     buffer = buffer if is_array else buffer[0]
     return buffer
 
+
 def gather_all(x):
-    is_array = isinstance(x, np.ndarray)
     x_buf = np.array([x])
     buffer = np.zeros_like(x_buf)
     buffer = np.repeat(buffer, get_num_procs(), axis=0)
     MPI.COMM_WORLD.Allgather(x_buf, buffer)
     buffer = list(buffer)
     return buffer
+
 
 def reduce_dict_mean(local_dict):
     keys = sorted(local_dict.keys())
